56e1eb9e3930e519606b8cd74fef35b48ee4fe01
Hasan-Error-Code <hasan.error.code@gmail.com>
2023-12-15 14:42:21 +0600

    max

--- max.py
+++ max.py
@@ -1,9 +1,8 @@
 from typing import Any
 
-num = [6,3,8,0,2,4,8,2,7,8,11,9,5,3,2,5,6,7]
+num = [6, 3, 8, 0, 2 , 4, 8, 2, 7, 8, 11, 9, 5, 3, 2, 5, 6, 7]
 
 max = num[0]
-i: int | Any
 for i in num: 
     if i > max:
         max = i
